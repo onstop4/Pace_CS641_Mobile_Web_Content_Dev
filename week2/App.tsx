0b22b4b59bbaa2a8f61c02ea4ff14fc1c2a9dad2
Andrew Henkel <search1230@gmail.com>
2024-09-19 19:27:34 -0400

    Added week1 and week2

---
+++ week2/App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello!</Text>
+      <StatusBar style="auto" />
+      {/* <ActivityIndicator size="large" /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
